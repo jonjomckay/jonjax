a8e3776cc186b970b59ff0a3bf68b21e028c0eef
Jonjo McKay <jonjo@jonjomckay.com>
2018-06-24 18:19:29 +0100

    Switched example to use Guice

--- jonjax-example/src/main/java/com/jonjax/example/ExampleApplication.java
+++ jonjax-example/src/main/java/com/jonjax/example/ExampleApplication.java
@@ -1,5 +1,7 @@
 package com.jonjax.example;
 
+import com.jonjax.container.ContainerBuilder;
+import com.jonjax.container.GuiceContainerBuilder;
 import com.jonjax.server.Server;
 import com.jonjax.server.undertow.UndertowServer;
 
@@ -7,8 +9,10 @@ import javax.ws.rs.core.Application;
 
 public class ExampleApplication extends Application {
     public static void main(String[] args) {
+        ContainerBuilder containerBuilder = new GuiceContainerBuilder();
+
         Server server = new UndertowServer();
         server.setApplication(new ExampleApplication());
-        server.start();
+        server.start(containerBuilder);
     }
 }
